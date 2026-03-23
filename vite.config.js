32836d03561c4d35e3abca6f06b815b91c071671
Jackson <jackson86427820@gmail.com>
2026-03-23 15:24:20 -0300

    Otimizando tamanho dos chunks no Vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  build: {
+    // Aumenta o limite do aviso para 1000kb (1MB)
+    chunkSizeWarningLimit: 1000, 
+    rollupOptions: {
+      output: {
+        // Separa as bibliotecas grandes (node_modules) em arquivos menores
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('lucide-react')) return 'vendor-icons';
+            if (id.includes('@supabase')) return 'vendor-supabase';
+            if (id.includes('react')) return 'vendor-react';
+            return 'vendor'; // O restante vai para um arquivo genérico
+          }
+        }
+      }
+    }
+  }
 })
